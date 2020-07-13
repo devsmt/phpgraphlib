89299050c8971cddb5a4fd0852125fd2124e8359
devsmt <devsmt@gmail.com>
2020-07-13 16:55:08 +0200

    correzioni

--- phpgraphlib.php
+++
@@ -1,3 +0,0 @@
-<?php
-$GLOBALS["HACKLIB_ROOT"] = '/usr/share/hhvm/hack/hacklib/hacklib.php';
-require_once __DIR__.'/build/phpgraphlib.php';
--- run_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm ./examples/*.png
-for i in {1..9}; do php ./examples/example$i.php;   done
